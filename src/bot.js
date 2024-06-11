4e694a61fe068b270dcc9956b28fa2a9001d5af6
Alexander Zatulyviter <oleksandr.zatulyviter@exilion.com>
2024-06-12 00:10:41 +0300

    move to bun

---
+++ src/bot.js
@@ -0,0 +1,22 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { isValidURL } from "./utils";
+
+export const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply("Welcome to BLANKS RADAR"));
+bot.on(message("text"), (ctx) => {
+  if (isValidURL(ctx.message.text)) {
+    fetch(new URL("localhost:3000/add-sub"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: ctx.message.text,
+      }),
+    }).then((r) => console.log(r));
+
+    ctx.reply("Subscription added succesfully");
+  }
+});
